2c7cd105c3bed6de3c118a749d2ae9203199daeb
Alex <prof.alex.etec.2021@gmail.com>
2024-08-27 21:33:02 -0300

    Adicionar arquivos de projeto.

---
+++ TDSN2024/FormaGeometrica.cs
@@ -0,0 +1,8 @@
+﻿namespace TDSN2024
+{
+    abstract class FormaGeometrica
+    {
+        public abstract double CalcularArea();
+        public abstract double CalcularPerimetro();
+    }
+}
---
+++ TDSN2024/Quadrado.cs
@@ -0,0 +1,23 @@
+﻿namespace TDSN2024
+{
+    class Quadrado : FormaGeometrica
+    {
+		private double lado;
+
+		public double Lado
+		{
+			get { return lado; }
+			set { lado = value; }
+		}
+
+        public override double CalcularArea()
+        {
+            return System.Math.Pow(lado, 2);
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return lado * 4;
+        }
+    }
+}
